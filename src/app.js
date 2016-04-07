05afe3ad3116383eff76db9f187f96e9cefb1326
Danielle Winter <Danielle Winter>
2016-04-07 14:35:52 +0200

    Added comments

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
   });
 
   var customers = [
-    { name: 'customer1', distance: 5, phone: 1234567890 , description: 'Airport', driverAssigned: true },
+    { name: 'customer1', distance: 5, phone: 1234567890 , description: 'Airport', driverAssigned: true }, 
     { name: 'customer2', distance: 2, phone: 0987654321 , description: 'Train Station', driverAssigned: false },
     { name: 'customer3', distance: 10, phone: 1029384756 , description: 'Library', driverAssigned: false }
   ];
